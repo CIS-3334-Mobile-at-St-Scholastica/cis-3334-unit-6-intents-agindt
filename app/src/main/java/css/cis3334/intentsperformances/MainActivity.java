4fe4f886df09979ef82dd9f2562fc5df18cadf81
agindt <agindt@css.edu>
2019-02-21 15:50:23 -0600

    Done with Practice 6.

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,7 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +13,7 @@ import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1Web, but1Calendar, btn1Map;
+    Button btn1Web, but1Calendar, btn1Map, btn2Web, but2Calendar, btn2Map;
     TextView textViewStatus;
 
     @Override
@@ -75,6 +76,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn2Web = (Button) findViewById(R.id.button2WebInfo);
+        btn2Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d129259274%26view%3devent%26-childview%3d");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the celendar button for the first performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but2Calendar = (Button) findViewById(R.id.button2Calendar);
+        but2Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent2 = new Intent(Intent.ACTION_INSERT);
+                calIntent2.setType("vnd.android.cursor.item/event");
+                calIntent2.putExtra(CalendarContract.Events.TITLE, "Red Green's \"This could be it!\" Tour");
+                calIntent2.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+                GregorianCalendar begDate = new GregorianCalendar(2019, 4, 7, 19, 00);
+                GregorianCalendar endDate = new GregorianCalendar(2019, 4, 7, 21, 00);
+                calIntent2.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent2.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent2);
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn2Map = (Button) findViewById(R.id.button2map);
+        btn2Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 1");
+                // Map point based on address
+                Uri location2 = Uri.parse("geo:0,0?q=1200+Kenwood+Ave,+Duluth,+Minnesota");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location2);
+            }
+        });
 
     }
 
